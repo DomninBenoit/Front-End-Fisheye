0dc1273dadbc1b6860b194efdb903bc4c45ee493
Domnin <dombouif@gmail.com>
2022-02-21 17:18:36 +0100

    Step 2 : Import Data + Step 3 : Embed homepage + start Step 4

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,54 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const {name, id, city, country, tagline, price, portrait} = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+        const lien = document.createElement("a");
+        lien.setAttribute("href", `photographer.html?id=${id}`);
+        lien.setAttribute("aria-label", name);
+        lien.classList.add(id);
+        const div = document.createElement('div');
+        const article = document.createElement('article');
+        const img = document.createElement('img');
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        const h2 = document.createElement('h2');
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        const p = document.createElement('p');
+        const localisation = document.createElement('span');
+        localisation.classList.add('city');
+        localisation.textContent = city + ", " + country;
+        const slogan = document.createElement('span');
+        slogan.classList.add('slogan');
+        slogan.textContent = tagline;
+        const rate = document.createElement('span');
+        rate.classList.add('price');
+        rate.textContent = price + '€/jour';
+        div.appendChild(img);
+        div.appendChild(h2);
+        lien.appendChild(div);
+        article.appendChild(lien);
+
+        p.appendChild(localisation);
+        p.appendChild(slogan);
+        p.appendChild(rate);
+        article.appendChild(p);
+
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+
+    function getUserCardDOMPage() {
+        const article = document.createElement("article");
+        const div = document.createElement("div");
+        const img = document.createElement('img');
+        img.setAttribute("src", picture)
+        const h1 = document.createElement("h1");
+        h1.textContent = name;
+        div.appendChild(img);
+        div.appendChild(h1);
+        article.appendChild(div);
+        return (article);
+    }
+
+    return {name, id, city, country, tagline, price, portrait, getUserCardDOM, getUserCardDOMPage};
 }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,35 @@
-//Mettre le code JavaScript lié à la page photographer.html
+const getPhotographers = async () => {
+
+
+    await fetch('./data/photographers.json')
+        .then((response) => response.json())
+        .then((data) => photographers = data.photographers)
+        .catch((error) => console.error(error));
+
+    return {
+        photographers: [...photographers],
+    };
+};
+
+const photographerDisplayData = () => {
+
+    const photographerHeader = document.querySelector(".photographer_header");
+    let params = (new URL(document.location)).searchParams;
+    let id = params.get('id');
+
+    photographers.forEach((photographer) => {
+        if (photographer.id === id) {
+            const photographerModelPage = photographerFactory(photographer);
+            const userCardDOMPage = photographerModelPage.getUserCardDOMPage();
+            photographerHeader.appendChild(userCardDOMPage);
+        }
+    });
+};
+
+async function initPage() {
+    // Récupère les datas des photographes
+    const { photographers } = await getPhotographers();
+    photographerDisplayData(photographers);
+}
+
+initPage();
